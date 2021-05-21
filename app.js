0901a42a8a1c89eae3b3246cdefeaacb52825070
Belastotski <di.belost@gmail.com>
2021-05-22 01:16:34 +0200

    fix: add port

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 var express = require('express');
-var app = express();
 var db = require('./db').sequelize;
 var user = require('./controllers/usercontroller');
 var game = require('./controllers/gamecontroller')
 
+var app = express();
 
 db.sync();
+app.use(express.json());
 app.use(require('body-parser'));
 app.use('/api/auth', user);
 app.use(require('./middleware/validate-session'))
 app.use('/api/game', game);
-app.listen(function() {
+
+app.listen(4000,function() {
     console.log("App is listening on 4000");
 })
